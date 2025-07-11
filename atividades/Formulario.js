1a67fd8f6dddd4b6cefc88f3aa3785d7b769e6ad
VitorGulicz <vdmgulicz@gmail.com>
2025-07-11 20:37:50 -0300

    update

---
+++ atividades/Formulario.js
@@ -0,0 +1,32 @@
+//executar mascaras
+function mascara(o,f){
+    //define o objeto e chama a funcao
+    objeto=o
+    funcao=f
+    setTimeout("executaMascara()",1)
+}
+
+function executaMascara(){
+    objeto.value=funcao(objeto.value)
+}
+
+//mascaras
+//telefone
+function telefone(variavel){
+    variavel=variavel.replace(/\D/g,"") //remove caracteres não numericos 
+    variavel=variavel.replace(/^(\d\d)(\d)/g,"($1)$2") //adiciona parenteses em volta dos dois primeiros digitos
+    variavel=variavel.replace(/(\d{4})(\d)/,"$1-$2") //adiciona hífen entre o quarto e quinto digitos
+    return variavel
+}
+
+
+
+function cnpj(variavel){
+    variavel=variavel.replace(/\D/g,"") //remove caracteres não numericos
+    variavel=variavel.replace(/(\d{2})(\d)/,"$1.$2") //adiciona ponto entre o segundo e terceiro digitos
+    variavel=variavel.replace(/(\d{3})(\d)/,"$1.$2") //adiciona ponto entre o sexto e setimo digitos
+    variavel=variavel.replace(/(\d{3})(\d)/,"$1/$2") //adiciona ponto entre o sexto e setimo digitos
+    variavel=variavel.replace(/(\d{4})(\d)/,"$1-$2") //adiciona ponto entre o sexto e setimo digitos
+    return variavel
+}
+
